e39b431d40fd68e4a6b125e8a292d48afd525bc3
Sean Washington <hello@seanwash.com>
2015-05-30 23:14:01 -0700

    Again, possible fix for duplicate redactors?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require redactor-rails
 //= require turbolinks
 //= require nprogress
 //= require nprogress-turbolinks
-//= require redactor-rails
 //= require_tree .
--- app/assets/javascripts/redactor-rails/config.js
+++ app/assets/javascripts/redactor-rails/config.js
@@ -6,18 +6,10 @@ window.init_redactor = function(){
 		params = csrf_param + "=" + encodeURIComponent(csrf_token);
 	}
 	$('.redactor').not('.redactified').redactor({
-		// You can specify, which ones plugins you need.
-		// If you want to use plugins, you have add plugins to your
-		// application.js and application.css files and uncomment the line below:
-		// "plugins": ['fontsize', 'fontcolor', 'fontfamily', 'fullscreen', 'textdirection', 'clips'],
-		// "imageUpload":"/redactor_rails/pictures?" + params,
-		// "imageGetJson":"/redactor_rails/pictures",
-		// "fileUpload":"/redactor_rails/documents?" + params,
-		// "fileGetJson":"/redactor_rails/documents",
 		"buttons": ['formatting', 'bold', 'italic', 'deleted', 'link'],
 		"path":"/assets/redactor-rails",
 		"css":"style.css"
 	}).addClass('redactified');
 };
 
-$(document).on( 'page:change', window.init_redactor );
+$(document).on( 'page:load page:change', window.init_redactor );
